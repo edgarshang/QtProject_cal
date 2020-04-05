867322953604732aaeb5c022fd407cebeb6165ed
edgarshang <edgarshang@163.com>
2020-04-05 19:34:03 +0800

    12 &&　splite the express

--- calculation/main.cpp
+++ calculation/main.cpp
@@ -1,18 +1,22 @@
 #include <QtGui/QApplication>
 #include "QCalculateUI.h"
+#include "QCalculatorDec.h"
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    int ret = -1;
-    QCalculateUI *cal = QCalculateUI::NewInstance();
-    if(cal != NULL)
-    {
+//    QApplication a(argc, argv);
+//    int ret = -1;
+//    QCalculateUI *cal = QCalculateUI::NewInstance();
+//    if(cal != NULL)
+//    {
 
-        cal->show();
-        ret = a.exec();
-        delete cal;
-    }
+//        cal->show();
+//        ret = a.exec();
+//        delete cal;
+//    }
 
+//    return ret;
+    int ret = 0;
+    QCalculatorDec dec;
     return ret;
 }
